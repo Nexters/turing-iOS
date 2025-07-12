6f9c1c86692b807e70b970706bac2e1d39d4f38e
koreamango <mango1229@icloud.com>
2025-07-12 13:26:28 +0900

    init

---
+++ turing/turingApp.swift
@@ -0,0 +1,17 @@
+//
+//  turingApp.swift
+//  turing
+//
+//  Created by koreamango on 7/12/25.
+//
+
+import SwiftUI
+
+@main
+struct turingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
