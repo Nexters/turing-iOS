a1bd4ef96fd874e1910d85c38fa61f43159a5026
koreamango <mango1229@icloud.com>
2025-07-20 20:39:28 +0900

    feat: Provider에 적용할 플러그인 추가

--- Gotchai/Core/Network/Sources/Plugin/NetworkLoggerPlugin.swift
+++ Gotchai/Core/Network/Sources/Plugin/NetworkLoggerPlugin.swift
@@ -5,3 +5,22 @@
 //  Created by koreamango on 7/20/25.
 //
 
+import Moya
+
+/// 요청, 응답 이후 Log를 남기는 Plugin
+final class NetworkLoggerPlugin: PluginType {
+  public init() {}
+
+  func willSend(_ request: RequestType, target: TargetType) {
+    print("➡️ 요청: \(request.request?.url?.absoluteString ?? "")")
+  }
+
+  func didReceive(_ result: Result<Response, MoyaError>, target: TargetType) {
+    switch result {
+    case .success(let response):
+        print("✅ 응답: \(response.statusCode), \(String(data: response.data, encoding: .utf8) ?? "")")
+    case .failure(let error):
+        print("❌ 에러: \(error)")
+    }
+  }
+}
