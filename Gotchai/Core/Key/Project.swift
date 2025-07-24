7074dd3150b1efb6a757fb063802b4c5ec4c85d9
koreamango <mango1229@icloud.com>
2025-07-25 01:47:04 +0900

    feat: key 모듈 추가

---
+++ Gotchai/Core/Key/Project.swift
@@ -0,0 +1,15 @@
+import ProjectDescription
+
+let project = Project(
+    name: "Key",
+    targets: [
+        .target(
+            name: "Key",
+            destinations: .iOS,
+            product: .framework,
+            bundleId: "com.gotchai.key",
+            sources: ["Sources/**"],
+            dependencies: []
+        )
+    ]
+)
