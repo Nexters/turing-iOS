c95c56a60c99d3a54acde44bc781fb00b840f9d6
koreamango <mango1229@icloud.com>
2025-07-27 11:37:55 +0900

    feat: fontstyle 축약형으로 바로 사용할 수 있게 수정

--- Gotchai/Shared/DesignSystem/Sources/GotchaiFont.swift
+++ Gotchai/Shared/DesignSystem/Sources/GotchaiFont.swift
@@ -1,34 +1,43 @@
-import SwiftRichString
-import Foundation
+import SwiftUI
 
-public enum Typography {
+public enum FontStyle {
+  case title_1, title_2, title_3, title_4
+  case subtitle_1, subtitle_2
+  case body_1, body_2, body_3, body_4, body_5, body_6
 
-    /// 공통 스타일 생성 함수
-    private static func style(font: FontConvertible,
-                              lineHeightMultiple: CGFloat = 1.5,
-                              kerningPercent: CGFloat = -3) -> Style {
-        return Style {
-            $0.font = font
-            $0.lineSpacing = lineHeightMultiple
-            $0.kerning = .adobe(kerningPercent * 10) // -3% → adobe -30
-        }
+  var font: Font {
+    switch self {
+    case .title_1: return Font(DesignSystemFontFamily.PretendardVariable.semiBold.font(size: 36))
+    case .title_2: return Font(DesignSystemFontFamily.PretendardVariable.bold.font(size: 30))
+    case .title_3: return Font(DesignSystemFontFamily.PretendardVariable.bold.font(size: 26))
+    case .title_4: return Font(DesignSystemFontFamily.PretendardVariable.semiBold.font(size: 24))
+
+    case .subtitle_1: return Font(DesignSystemFontFamily.PretendardVariable.semiBold.font(size: 22))
+    case .subtitle_2: return Font(DesignSystemFontFamily.PretendardVariable.semiBold.font(size: 20))
+
+    case .body_1: return Font(DesignSystemFontFamily.PretendardVariable.medium.font(size: 18))
+    case .body_2: return Font(DesignSystemFontFamily.PretendardVariable.semiBold.font(size: 18))
+    case .body_3: return Font(DesignSystemFontFamily.PretendardVariable.semiBold.font(size: 16))
+    case .body_4: return Font(DesignSystemFontFamily.PretendardVariable.medium.font(size: 16))
+    case .body_5: return Font(DesignSystemFontFamily.PretendardVariable.medium.font(size: 15))
+    case .body_6: return Font(DesignSystemFontFamily.PretendardVariable.medium.font(size: 14))
     }
+  }
+}
 
-    // MARK: - Titles
-    public static let title1 = style(font: DesignSystemFontFamily.PretendardVariable.semiBold.font(size: 36))
-    public static let title2 = style(font: DesignSystemFontFamily.PretendardVariable.bold.font(size: 30))
-    public static let title3 = style(font: DesignSystemFontFamily.PretendardVariable.bold.font(size: 26))
-    public static let title4 = style(font: DesignSystemFontFamily.PretendardVariable.semiBold.font(size: 24))
+extension Font {
+  static var title_1: Font { FontStyle.title_1.font }
+  static var title_2: Font { FontStyle.title_2.font }
+  static var title_3: Font { FontStyle.title_3.font }
+  static var title_4: Font { FontStyle.title_4.font }
 
-    // MARK: - Subtitles
-    public static let subtitle1 = style(font: DesignSystemFontFamily.PretendardVariable.semiBold.font(size: 22))
-    public static let subtitle2 = style(font: DesignSystemFontFamily.PretendardVariable.semiBold.font(size: 20))
+  static var subtitle_1: Font { FontStyle.subtitle_1.font }
+  static var subtitle_2: Font { FontStyle.subtitle_2.font }
 
-    // MARK: - Body
-    public static let body1 = style(font: DesignSystemFontFamily.PretendardVariable.medium.font(size: 18))
-    public static let body2 = style(font: DesignSystemFontFamily.PretendardVariable.semiBold.font(size: 18))
-    public static let body3 = style(font: DesignSystemFontFamily.PretendardVariable.semiBold.font(size: 16))
-    public static let body4 = style(font: DesignSystemFontFamily.PretendardVariable.medium.font(size: 16))
-    public static let body5 = style(font: DesignSystemFontFamily.PretendardVariable.medium.font(size: 15))
-    public static let body6 = style(font: DesignSystemFontFamily.PretendardVariable.medium.font(size: 14))
+  static var body_1: Font { FontStyle.body_1.font }
+  static var body_2: Font { FontStyle.body_2.font }
+  static var body_3: Font { FontStyle.body_3.font }
+  static var body_4: Font { FontStyle.body_4.font }
+  static var body_5: Font { FontStyle.body_5.font }
+  static var body_6: Font { FontStyle.body_6.font }
 }
