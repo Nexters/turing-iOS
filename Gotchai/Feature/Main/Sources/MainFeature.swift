fb093567563d1ae261c9ae574a450c60394b319d
slr-09 <ge12090@naver.com>
2025-08-02 00:58:32 +0900

    UI: Main view 테스트 리스트 화면 구현

--- Gotchai/Feature/Main/Sources/MainFeature.swift
+++ Gotchai/Feature/Main/Sources/MainFeature.swift
@@ -13,6 +13,11 @@ public struct MainFeature {
     @ObservableState
     public struct State {
         var selectedTab: Tab = .turingTest
+        var turingTestItems: [TuringTestCard] = [
+            .init(id: 1, imageURL: "", title: "Ai와 크리스마스 파티", subtitle: "산타는 누구야?"),
+            .init(id: 2, imageURL: "", title: "소개팅 톡 감별 테스트", subtitle: "어떤 대답이 진심일까?"),
+            .init(id: 3, imageURL: "", title: "전학생과의 첫 인사", subtitle: "누가 진짜 전학생이야?")
+        ]
     }
     
     public enum Action {
--- Gotchai/Feature/Main/Sources/MainView.swift
+++ Gotchai/Feature/Main/Sources/MainView.swift
@@ -35,7 +35,7 @@ struct MainView: View {
                             switch tab {
                             case .turingTest:
                                 ScrollView {
-                                    testCardList()
+                                    TestCardList()
                                 }
                             case .achievement:
                                 // TODO: 업적(Profile) 연결
@@ -52,20 +52,28 @@ struct MainView: View {
     }
     
     @ViewBuilder
-    private func testCardList() -> some View {
+    private func TestCardList() -> some View {
         VStack(alignment: .leading, spacing: 0) {
             Image("icon_ab", bundle: .module)
             Text("테스트")
                 .font(.body_2)
                 .foregroundStyle(DesignSystemAsset.grayWhite.swiftUIColor)
                 .padding(.top, 12)
-            Text("13개의 새로운 테스트가 있어요")
+            Text("\(store.turingTestItems.count)개의 새로운 테스트가 있어요")
                 .font(.body_4)
                 .foregroundStyle(DesignSystemAsset.gray400.swiftUIColor)
                 .padding(.top, 2)
             
-            
+            ForEach(store.turingTestItems, id: \.id) { item in
+                Button {
+                    
+                } label: {
+                    TestCardItem(item: item)
+                }
+            }
+            .padding(.top, 16)
         }
+        .frame(maxWidth: .infinity, alignment: .leading)
         .padding(.vertical, 20)
         .padding(.horizontal, 16)
         .background(
@@ -73,6 +81,36 @@ struct MainView: View {
                 .fill(DesignSystemAsset.gray900.swiftUIColor)
         )
         .padding(.top, 16)
+    }
+    
+    @ViewBuilder
+    private func TestCardItem(item: TuringTestCard) -> some View {
+        HStack(spacing: 16) {
+            AsyncImage(url: URL(string: item.imageURL)) { image in
+                image.resizable()
+            } placeholder: {
+//                ProgressView()
+            }
+            .frame(width: 80, height: 80)
+            .background(
+                Circle().fill(DesignSystemAsset.gray900.swiftUIColor)
+            )
+
+            VStack(alignment: .leading, spacing: 2) {
+                Text(item.title)
+                    .font(.body_2)
+                    .foregroundStyle(DesignSystemAsset.grayWhite.swiftUIColor)
+                Text(item.subtitle)
+                    .font(.body_4)
+                    .foregroundStyle(DesignSystemAsset.gray400.swiftUIColor)
+            }
+            Spacer()
+        }
+        .padding(10)
+        .background(
+            RoundedRectangle(cornerRadius: 20)
+                .fill(DesignSystemAsset.gray800.swiftUIColor)
+        )
     }
 }
 
