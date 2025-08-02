40874dbe1ff94b240d9792ca36a60fe1813ec259
koreamango <mango1229@icloud.com>
2025-08-02 15:10:22 +0900

    fix: tca 버전 수정

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     name: "Gotchai",
     dependencies: [
       .package(url: "https://github.com/kakao/kakao-ios-sdk", from: "2.24.5"),
-      .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "1.0.0"),
+      .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "1.17.0"),
       .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: "15.0.0")),
     ],
     targets: [
