48f5c7b28348e702fd145c49e21b761b7af61586
koreamango <mango1229@icloud.com>
2025-08-02 21:09:23 +0900

    fix: Sign In 화면 디테일 수정

--- Gotchai/Feature/SignIn/Sources/SignInView.swift
+++ Gotchai/Feature/SignIn/Sources/SignInView.swift
@@ -8,63 +8,69 @@
 import SwiftUI
 import ComposableArchitecture
 import DesignSystem
+import Auth
 
 public struct SignInView: View {
-  let store: StoreOf<SignInFeature>
+    let store: StoreOf<SignInFeature>
 
-  public init(store: StoreOf<SignInFeature>) {
-    self.store = store
-  }
+    public init(store: StoreOf<SignInFeature>) {
+        self.store = store
+    }
 
-  public var body: some View {
-    WithViewStore(self.store, observe: { $0 }) { viewStore in
-      VStack(spacing: 40) {
-        Spacer()
-
-        VStack(spacing: 16) {
-          Image("logo", bundle: .module)
-
-          Text("인간 사이 숨은 AI 찾기")
-            .foregroundColor(.gray)
-        }
-
-        Spacer()
-
-        VStack(spacing: 16) {
-          Button {
-            viewStore.send(.tappedKakaoLogin)
-          } label: {
-            HStack {
-              Image(systemName: "message.fill")
+    public var body: some View {
+        WithViewStore(self.store, observe: { $0 }) { viewStore in
+            VStack {
+                Spacer()
+                VStack(spacing: 4) {
+                    Image("logo", bundle: .module)
+                    Text("인간 사이 숨은 AI 찾기")
+                        .foregroundColor(Color(.gray_200))
+                }
+                Spacer(minLength: 300)
+                VStack(spacing: 16) {
+                    Button {
+                        viewStore.send(.tappedKakaoLogin)
+                    } label: {
+                        HStack {
+                            Image(systemName: "message.fill")
               Text("카카오로 시작하기")
                 .bold()
             }
             .frame(maxWidth: .infinity)
             .padding()
-            .background(Color.yellow)
+            .background(Color(UIColor(red: 254, green: 229, blue: 0, alpha: 1)))
             .foregroundColor(.black)
             .cornerRadius(40)
-          }
-
-          Button {
-            viewStore.send(.tappedAppleLogin)
-          } label: {
-            HStack {
-              Image(systemName: "apple.logo")
-              Text("Apple로 시작하기")
             }
-            .frame(maxWidth: .infinity)
-            .padding()
-            .background(DesignSystemAsset.gray800.swiftUIColor)
-            .foregroundColor(.white)
-            .cornerRadius(40)
-          }
+
+            Button {
+                viewStore.send(.tappedAppleLogin)
+            } label: {
+                HStack {
+                    Image(systemName: "apple.logo")
+                    Text("Apple로 시작하기")
+                }
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(Color(.gray_800))
+                .foregroundColor(Color(.gray_white))
+                .cornerRadius(50)
+            }
         }
         .padding(.horizontal, 24)
         .padding(.bottom, 40)
-      }
-      .background(DesignSystemAsset.gray950.swiftUIColor)
+            }
+            .background(Color(.gray_950))
+        }
     }
-  }
+}
+
+#Preview {
+    SignInView(store: StoreOf<SignInFeature>(
+        initialState: SignInFeature.State(),
+        reducer: {
+            SignInFeature(kakaoAuthProvider: KakaoAuthProvider(appKey: ""), appleAuthProvider: AppleAuthProvider())
+        })
+    )
 }
 
