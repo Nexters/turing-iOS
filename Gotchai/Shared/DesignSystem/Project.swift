7ba394b61d2c18e11e56fa36fbb58517b9f5118a
slr-09 <ge12090@naver.com>
2025-08-05 02:04:17 +0900

    chore: project.swift에 불필요한  setting 코드 삭제

--- Gotchai/Shared/DesignSystem/Project.swift
+++ Gotchai/Shared/DesignSystem/Project.swift
@@ -12,16 +12,7 @@ let project = Project(
             bundleId: "com.gotchai.design-system",
             sources: ["Sources/**"],
             resources: ["Resources/**"],
-            dependencies: [],
-            settings: .settings(
-                base: [
-                    "IPHONEOS_DEPLOYMENT_TARGET": "17.0",
-                    "DEVELOPMENT_TEAM": "$(DEVELOPMENT_TEAM)"
-                ],
-                configurations: [
-                    .debug(name: "Debug", xcconfig: "../../../Tuist/Configurations/Config.xcconfig"),
-                    .release(name: "Release", xcconfig: "../../../Tuist/Configurations/Config.xcconfig")
-            ])
+            dependencies: []
         )
     ]
 )
