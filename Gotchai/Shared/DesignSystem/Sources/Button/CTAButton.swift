feab056b28df3024e5e312c715c02a8129976446
slr-09 <ge12090@naver.com>
2025-08-04 20:41:11 +0900

    UI: CTAButton 제작

---
+++ Gotchai/Shared/DesignSystem/Sources/Button/CTAButton.swift
@@ -0,0 +1,31 @@
+//
+//  CTAButton.swift
+//  DesignSystem
+//
+//  Created by 가은 on 8/4/25.
+//
+
+import SwiftUI
+
+public struct CTAButton: View {
+    let text: String
+    let action: () -> Void
+    
+    public var body: some View {
+        Button(action: action) {
+            Text(text)
+                .frame(maxWidth: .infinity)
+                .font(.body_2)
+                .foregroundStyle(.grayBlack)
+                .padding(.vertical, 15)
+        }
+        .background(
+            RoundedRectangle(cornerRadius: 16)
+                .fill(.primary400)
+        )
+    }
+}
+
+#Preview {
+    CTAButton(text: "다음", action: { })
+}
