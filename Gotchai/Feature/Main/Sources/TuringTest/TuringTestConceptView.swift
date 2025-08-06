9f1b549794fa0fd7d8cf642967b20587d9a7f652
slr-09 <ge12090@naver.com>
2025-08-07 00:38:56 +0900

    UI: 테스트 컨셉 화면 배경 그라데이션 추가

--- Gotchai/Feature/Main/Sources/TuringTest/TuringTestConceptView.swift
+++ Gotchai/Feature/Main/Sources/TuringTest/TuringTestConceptView.swift
@@ -13,29 +13,58 @@ struct TuringTestConceptView: View {
     let store: StoreOf<TuringTestFeature>
     
     var body: some View {
-        VStack {
-            Text("""
+        ZStack {
+            // TODO: 배경 이미지 추가 
+            
+            Color(.gray_950).opacity(0.5).ignoresSafeArea()
+            
+            BackgroundGradient().ignoresSafeArea()
+            
+            VStack {
+                Text("""
                 \(store.turingTest.explanation)
                 """)
-            .fontStyle(.body_1)
-            .foregroundStyle(Color(.gray_white))
-            .multilineTextAlignment(.center)
-            .padding(.top, 32)
-            
-            Spacer()
-            
-            CTAButton(text: "다음") {
-                store.send(.moveToQuizView)
+                .fontStyle(.body_1)
+                .foregroundStyle(Color(.gray_white))
+                .multilineTextAlignment(.center)
+                .padding(.top, 32)
+                
+                Spacer()
+                
+                CTAButton(text: "다음") {
+                    store.send(.moveToQuizView)
+                }
+                .padding(.horizontal, 24)
+                .padding(.bottom, 10)
             }
-            .padding(.horizontal, 24)
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Image("icon_xmark", bundle: .module)
+                        .padding(12)
+                }
+            }
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color(.gray_950))   // TODO: 이미지로 변경
-        .toolbar {
-            ToolbarItem(placement: .topBarTrailing) {
-                Image("icon_xmark", bundle: .module)
-                    .padding(12)
-            }
+    }
+    
+    @ViewBuilder
+    private func BackgroundGradient() -> some View {
+        VStack(spacing: 108) {
+            LinearGradient(
+                gradient: Gradient(stops: [
+                    .init(color: Color(.gray_950), location: 0.0),
+                    .init(color: Color(.gray_950).opacity(0.7), location: 0.7),
+                    .init(color: Color(.gray_950).opacity(0.0), location: 1.0)
+                ]),
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            
+            LinearGradient(
+                colors: [Color(.gray_950), Color(.gray_950).opacity(0.0)],
+                startPoint: .bottom,
+                endPoint: .top
+            )
+            .frame(height: 160)
         }
     }
 }
