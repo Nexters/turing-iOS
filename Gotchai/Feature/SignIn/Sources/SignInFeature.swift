a5dcd6745453a53bfdaa90fc2aa5e723f7f13880
koreamango <mango1229@icloud.com>
2025-08-09 02:08:55 +0900

    chore: SignIn Feature 줄바꿈 수정

--- Gotchai/Feature/SignIn/Sources/SignInFeature.swift
+++ Gotchai/Feature/SignIn/Sources/SignInFeature.swift
@@ -16,55 +16,55 @@ public struct SignInFeature {
     @Dependency(\.appRouter) var appRouter
     @Dependency(\.signInClient) var signInClient
 
-  @ObservableState
-  public struct State: Equatable {
-    public init() { }
-    var isLoading: Bool = false
-    var user: UserSession?
-  }
-
-  public enum Action {
-    case tappedKakaoLogin
-    case tappedAppleLogin
-    case signInResponse(Result<UserSession, Error>)
-  }
-
-  public init() {}
-
-  public var body: some ReducerOf<Self> {
-    Reduce { state, action in
-      switch action {
-      case .tappedKakaoLogin:
-        state.isLoading = true
-        
-        return .run { send in
-          let session = try await signInClient.signInWithKakao(kakaoAuthProvider)
-          await send(.signInResponse(.success(session)))
-        } catch: { error, send in
-          await send(.signInResponse(.failure(error)))
-        }
-        
-      case .tappedAppleLogin:
-        state.isLoading = true
-        return .run { send in
-          let session = try await signInClient.signInWithApple(appleAuthProvider)
-          await send(.signInResponse(.success(session)))
-        } catch: { error, send in
-          await send(.signInResponse(.failure(error)))
-        }
-        
-      case let .signInResponse(.success(session)):
-        state.isLoading = false
-        print("로그인 성공: \(session)")
-        return .none
-        
-      case let .signInResponse(.failure(error)):
-        state.isLoading = false
-        print(error)
-        return .none
-      }
+    @ObservableState
+        public struct State: Equatable {
+        public init() { }
+        var isLoading: Bool = false
+        var user: UserSession?
     }
-  }
+
+    public enum Action {
+        case tappedKakaoLogin
+        case tappedAppleLogin
+        case signInResponse(Result<UserSession, Error>)
+    }
+
+    public init() {}
+
+    public var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case .tappedKakaoLogin:
+                state.isLoading = true
+        
+                return .run { send in
+                    let session = try await signInClient.signInWithKakao(kakaoAuthProvider)
+                    await send(.signInResponse(.success(session)))
+                } catch: { error, send in
+                    await send(.signInResponse(.failure(error)))
+                }
+        
+            case .tappedAppleLogin:
+                state.isLoading = true
+                return .run { send in
+                    let session = try await signInClient.signInWithApple(appleAuthProvider)
+                    await send(.signInResponse(.success(session)))
+                } catch: { error, send in
+                    await send(.signInResponse(.failure(error)))
+                }
+        
+            case let .signInResponse(.success(session)):
+                state.isLoading = false
+                print("로그인 성공: \(session)")
+                return .none
+        
+            case let .signInResponse(.failure(error)):
+                state.isLoading = false
+                print(error)
+                return .none
+            }
+        }
+    }
 }
 
 
