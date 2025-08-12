d9fc1602a7d7f504cce7e4d5354c1926ca502270
slr-09 <ge12090@naver.com>
2025-08-12 23:52:56 +0900

    feat: Setting 모듈 생성

---
+++ Gotchai/Feature/Setting/Sources/SettingView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingView.swift
+//  Setting
+//
+//  Created by 가은 on 8/12/25.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingView()
+}
